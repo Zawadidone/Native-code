0e40140b16675f3fcea366d27986cd32b4bb619e
ZawadiDone <z.done@nfir.nl>
2019-07-11 15:38:55 +0200

    rename dir

--- c/c-programmig/README.md
+++
@@ -1,4 +0,0 @@
-# [C Programming](https://en.wikibooks.org/wiki/C_Programming)
-
-
-note: I know `gets()` and `scanf()` are not secure
--- c/c-programmig/scope.c
+++
@@ -1,9 +0,0 @@
-int i = 5; /* this is a 'global' variable, it can be accessed from anywhere in the program */
-
-/* this is a function, all variables inside of it
- * are "local" to the function. */
-int main(void)
-{
-    int i = 6; /* 'i' now equals 6 */
-    printf("%d\n", i); /* prints a '6' to the screen, instead of the global variable of 'i', which is 5 */
-}
